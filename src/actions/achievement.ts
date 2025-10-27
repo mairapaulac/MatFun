82a4d2b911a238b5f8d49386fa0de611a1e0a0c5
Francisco Sérgio Feitosa <fsfsegundo@hotmail.com>
2025-10-26 21:02:53 -0300

    refactor(security): adjusted getUserAchievments action, it now requires valid JWT token

--- src/actions/achievement.ts
+++ src/actions/achievement.ts
@@ -1,20 +1,17 @@
 "use server";
+import { verifySession } from "./session";
 
 const apiUrl = process.env.API_BASE_URL;
 
 export default async function getUserAchievements(userId: number) {
-  const response = await fetch(`${apiUrl}/achievements/me?userId=${userId}`);
+  const {token} =await verifySession(); 
+  const response = await fetch(`${apiUrl}/achievements/me?userId=${userId}`, {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	});
   if (!response.ok) {
-    let errorMessage = "Failed to fetch achievements";
-    const contentType = response.headers.get("Content-Type");
-
-    if (contentType && contentType.includes("application/json")) {
-      const errorData = await response.json();
-      errorMessage = errorData.message || JSON.stringify(errorData);
-    } else {
-      errorMessage = await response.text();
-    }
-    throw new Error(errorMessage);
+    throw new Error( "Failed to fetch achievements");
   }
 
   const data = await response.json();
