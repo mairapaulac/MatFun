d6375992591b3cd20378cd312764357bf5736be5
Francisco Sérgio Feitosa <fsfsegundo@hotmail.com>
2025-11-12 19:22:13 -0300

    added the rest of proxies

---
+++ src/app/api/support/anos/[gradeId]/turmas/route.ts
@@ -0,0 +1,30 @@
+
+import { NextRequest, NextResponse } from "next/server";
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { gradeId: string } }
+) {
+  const gradeId = params.gradeId;
+
+  try {
+    const result = await fetch(`${apiUrl}/support/anos/${gradeId}/turmas`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await result.json();
+    if (result.ok) {
+      return NextResponse.json(data, { status: 200 });
+    }
+    return NextResponse.json({ error: "erro" }, { status: result.status });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { error: "Erro interno no servidor." },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/support/escolas/[schoolId]/anos/route.ts
@@ -0,0 +1,30 @@
+
+import { NextRequest, NextResponse } from "next/server";
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { schoolId: string } }
+) {
+  const schoolId = params.schoolId;
+
+  try {
+    const result = await fetch(`${apiUrl}/support/escolas/${schoolId}/anos`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await result.json();
+    if (result.ok) {
+      return NextResponse.json(data, { status: 200 });
+    }
+    return NextResponse.json({ error: "erro" }, { status: result.status });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { error: "Erro interno no servidor." },
+      { status: 500 }
+    );
+  }
+}
