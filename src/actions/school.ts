bbd7a0bc0bbb075bdc71d2bc98562d88d8f0aa6e
Francisco Sérgio Feitosa <fsfsegundo@hotmail.com>
2025-11-12 20:02:36 -0300

    s

--- src/actions/school.ts
+++ src/actions/school.ts
@@ -3,16 +3,15 @@
 export async function getSchools() {
 
 	const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/school`);
-
 	const data = await res.json();
-	return data;
+	return data.data;
 }
 export async function getGradesFromSchools(schoolId:number) {
 
 	const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/support/escolas/${schoolId}/anos`);
 
 	const data = await res.json();
-	return data;
+	return data.data;
 }
 
 //fazer dropdown disso aqui abaixo
@@ -21,5 +20,5 @@ export async function getClassesFromGrade(gradeId:number ) {
 	const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/support/anos/${gradeId}/turmas`) 
 
 	const data = await res.json();
-	return data;
+	return data.data;
 }
